bb61b8008f21afe4d3975e458c900703e54917d9
marcelojcn <marcelojcnunes@hotmail.com>
2017-11-14 14:51:24 +0000

    adding the client and server of a ssh, implementation still doesnt have anything to do with the real project

---
+++ ssh-rmi-client/src/main/java/client/User.java
@@ -0,0 +1,90 @@
+package client;
+
+import java.rmi.*;
+import java.util.Scanner;
+
+import server.TTTService;
+
+public class User {
+	TTTService ttt;
+	Scanner keyboardSc;
+	int winner = 0;
+	int player = 1;
+	
+	public User(TTTService stub) throws RemoteException {
+		ttt = stub;
+		keyboardSc = new Scanner(System.in);
+	}
+
+	public int readPlay() {
+		int play;
+		do {
+			System.out.printf("\nPlayer %d, please enter the number of the square "
+							+ "where you want to place your %c (or 0 to refresh the board): \n",
+							player, (player == 1) ? 'X' : 'O');
+			play = keyboardSc.nextInt();
+		} while (play > 10 || play < 0);
+		return play;
+	}
+
+	public void playGame() throws RemoteException {
+		int play;
+		boolean playAccepted;
+
+		do {
+			player = ++player % 2;
+			do {
+				System.out.println(ttt.currentBoard());
+				play = readPlay();
+				if (play > 0 && play < 10) {
+					playAccepted = ttt.play( --play / 3, play % 3, player);
+					if (!playAccepted)
+						System.out.println("Invalid play! Try again.");
+				} 
+				else if(play == 10){
+					ttt.reiniciar();
+					playAccepted = false;
+				}else
+					playAccepted = false;
+			} while (!playAccepted);
+			winner = ttt.checkWinner();
+		} while (winner == -1);
+	}
+
+	public void congratulate() {
+		int play;
+		if (winner == 2)
+			System.out.printf("\nHow boring, it is a draw\n");
+		else
+			System.out.printf(
+					"\nCongratulations, player %d, YOU ARE THE WINNER!\n",
+					winner);
+		
+		do {
+			System.out.printf("10 para reiniciar; \n");
+			play = keyboardSc.nextInt();
+		} while (play != 10);
+	}
+	
+	
+    public static void main(String[] args) throws Exception {
+
+		TTTService server = null;
+		try {
+			server = (TTTService) Naming.lookup(args[0]);
+			System.out.println("Found server");
+
+			while(true){
+				User g = new User(server);
+				
+				g.playGame();
+				g.congratulate();
+			}
+			
+			
+			} catch (Exception e) {
+				System.out.println("Lookup: " + e.getMessage());
+		}
+
+    }
+}
---
+++ ssh-rmi-server/src/main/java/server/Gateway.java
@@ -0,0 +1,33 @@
+package server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Gateway {
+
+	public static void main(String[] args){
+		String name = args[0];
+		int registryPort = Integer.parseInt(args[1]);
+		
+        System.out.println("Main OK");
+        
+        try{
+            TTTService _ttt = new TTT();
+            System.out.println("After create");
+            
+            Registry reg = LocateRegistry.createRegistry(registryPort);
+			reg.rebind(name, _ttt);			
+           
+            System.out.println("TTT server ready");
+
+            System.out.println("Awaiting connections");
+            System.out.println("Press enter to shutdown");
+            System.in.read();
+            
+        }catch(Exception e) {
+            System.out.println("TTT server main " + e.getMessage());
+        }
+
+	}
+
+}
