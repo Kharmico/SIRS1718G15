eb950916b10ab1bc9453cd324193fbd25699daba
marcelojcn <marcelojcnunes@hotmail.com>
2017-11-20 15:49:19 +0000

    command line working

--- ssh-rmi-client/src/main/java/client/User.java
+++ ssh-rmi-client/src/main/java/client/User.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.rmi.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,11 +13,11 @@ import server.Services.GatewayService;
 public class User {
 	private SecretKey serverPublicKey;
 	private GatewayService gateway;
-	private Scanner keyboardSc;
+	public Boolean adminFlag;
+	
 	
 	public User(GatewayService stub) throws RemoteException {
 		gateway = stub;
-		keyboardSc = new Scanner(System.in);
 	}
 
 	public String RegisterUser(String name, String password) throws RemoteException {
@@ -50,8 +51,8 @@ public class User {
 		
 	}
 
-	public String SendCommand(String command) throws RemoteException {
-		String response = gateway.SendCommand(null, null, command);
+	public String SendCommand(String deviceName, String command) throws RemoteException {
+		String response = gateway.SendCommand(null, deviceName,command);
 		
 		return response;
 	}
@@ -83,13 +84,101 @@ public class User {
 	
     public static void main(String[] args) throws Exception {
 
+    	StringBuilder sb = new StringBuilder();
+    	
+    	String input;
+    	String[] parsedInput;
+    	
+    	Scanner keyboardSc = new Scanner(System.in);
+    	
     	GatewayService server = null;
-		try {
+		
+    	try {
 			server = (GatewayService) Naming.lookup(args[0]);
 			System.out.println("Found server");
+			User g = new User(server);
 
-			while(true){
-				User g = new User(server);
+			ioLoop: while(true){
+				input = keyboardSc.nextLine();
+				
+				parsedInput = input.split(" ");
+				
+				switch(parsedInput[0]) {
+
+				case "devices":
+					g.GetDeviceStatus();
+					break;
+
+				case "commands":
+					g.GetDeviceCommands(parsedInput[1]);
+					break;
+
+				case "request":					
+					String command = String.join(" ", Arrays.copyOfRange(parsedInput, 3, parsedInput.length));
+					
+					g.SendCommand(parsedInput[1], command);
+					break;
+
+				case "login":
+					if(parsedInput.length == 3) {
+						g.Login(parsedInput[1], parsedInput[2], null);
+					}
+					else if(parsedInput.length == 4) {
+						g.Login(parsedInput[1], parsedInput[2], parsedInput[3]);
+					}
+					else {
+						System.out.println("Unrecognized login command");
+					}
+					break;
+
+				case "register":
+
+					if(parsedInput.length == 3) {
+						g.RegisterUser(parsedInput[1], parsedInput[2]);
+					}
+					else {
+						System.out.println("Unrecognized register command");
+					}
+					break;
+
+				case "delete":
+
+					if(parsedInput.length == 2) {
+						g.DeleteUser(parsedInput[1]);
+					}
+					else {
+						System.out.println("Unrecognized delete command");
+					}
+					
+					break;
+
+				case "exit":
+					break ioLoop;
+					
+				case "help":
+						System.out.println("devices - for existing devices and their status\n"
+								+ ">> devices");
+						System.out.println("commands - request the commands available for a device\n"
+								+ ">> commands deviceName");
+						System.out.println("request - to request a command on a device\n"
+								+ ">> request deviceName -c command");
+						System.out.println("login - used to start a session on the gateway\n"
+								+ ">> login username password [authenticationString]");
+						System.out.println("register - [ADMIN ONLY]register a new user in the network\n"
+								+ ">> register username password");
+						System.out.println("delete - [AMIN ONLY]delete a user from the network\n"
+								+ ">> delete username");
+						System.out.println("help - for existing commands\n"
+								+ ">> help");
+						System.out.println("exit - to exit the client program\n"
+								+ ">> exit");
+						break;
+				
+					default:
+						System.out.println("Command not recognized");
+						break;
+				}
+				
 			}
 			
 			
