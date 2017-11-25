4c157f8c977f8543f1683e3bae065dbf9ed2f9cd
Kharmico <phoenix-2000@hotmail.com>
2017-11-25 18:48:44 +0000

    Added initial nounce verification, to be completed

--- ssh-rmi-server/src/main/java/server/Controllers/GatewayController.java
+++ ssh-rmi-server/src/main/java/server/Controllers/GatewayController.java
@@ -1,10 +1,12 @@
 package server.Controllers;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.Map;
 import javax.crypto.SecretKey;
 
 import server.Services.GatewayService;
+import utils.EncryptionUtil;
 
 public class GatewayController extends UnicastRemoteObject implements GatewayService {
 
@@ -21,13 +24,29 @@ public class GatewayController extends UnicastRemoteObject implements GatewaySer
 	public List<ArrayList<String>> devices = new ArrayList<ArrayList<String>>();
 	private ServerSocket registerSocket;
 	private Map<String, Socket> devConnections = new HashMap<String,Socket>();
+	private ArrayList<String> nonceList = new ArrayList<String>();
+	private EncryptionUtil encUtil = new EncryptionUtil();
 
 
 	// GatewayController Constructor
 	public GatewayController() throws RemoteException {}
 
 	//GatewayController Endpoints
+	
 	public List<byte[]> RegisterUser(byte[] adminUsername, byte[] adminPassword, byte[] name, byte[] password, byte[] nonce, byte[] signature) {
+		String str_nonce;
+		byte[] dec_nonce = encUtil.decrypt(nonce);
+		try {
+			// nonce%timestamp
+			str_nonce = new String(dec_nonce, "UTF-8");
+			String[] strings_nonce = str_nonce.split("%");
+			if(nonceList.contains(str_nonce) || checkTimestamp(strings_nonce[1])){
+				System.out.println("NONCE ALREADY SEEN!!!");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
 		
 		// TODO Auto-generated method stub
 		return null;
@@ -81,13 +100,33 @@ public class GatewayController extends UnicastRemoteObject implements GatewaySer
 	         
 	     }
 	}
+	
 	public int createListeningSocket(int port) throws IOException{
 
 		registerSocket = new ServerSocket(port); 		//If port == 0 find an available port to listen for new devices registration
 		return registerSocket.getLocalPort();			//Else listen on the requested port
 	}
+	
 	public void closeListeningDevicesSocket() throws IOException{
 		registerSocket.close();
+	}
+	
+	// (yyyy/mm/dd hh:mm:ss)
+	private boolean checkTimestamp(String timest){
+		LocalDateTime currentTime;
+		String[] divide = timest.split("[ /:]");
+		int[] timestCalendar = new int[3];
+		int[] timestHours = new int[3];
+		
+		for(int i = 0; i < 3; i++){
+			timestCalendar[i] = Integer.parseInt(divide[i]);
+			timestHours[i] = Integer.parseInt(divide[i+3]);
+		}
+		
+		currentTime = LocalDateTime.now();
+		//LocalDateTime has functions to get the year, month, day, hour, minute, second from it, just need to do a manual diff, no need
+		//to waste time, resources, computation on extra shit...
+		return true;
 	}
 
 }
