f278bc48b871aaf7506b2f761463e77e5ecd99d8
marcelojcn <marcelojcnunes@hotmail.com>
2017-11-25 18:27:49 +0000

    security requisites

--- ssh-rmi-client/src/main/java/client/User.java
+++ ssh-rmi-client/src/main/java/client/User.java
@@ -1,11 +1,20 @@
 package client;
 
+import java.io.UnsupportedEncodingException;
 import java.rmi.*;
+import java.security.SignatureException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import javax.crypto.SecretKey;
 
@@ -13,22 +22,72 @@ import server.Services.GatewayService;
 import utils.EncryptionUtil;
 
 public class User {
-	private SecretKey serverPublicKey;
-	private GatewayService gateway;
-	public Boolean adminFlag;
+	//Static
+	private static String UTF8 = "UTF-8";
 	
+	
+	//Encryption
+	private EncryptionUtil encryption;
+	private ArrayList<String> nounces = new ArrayList<String>();
+	private Date cleanSchedule = new Date();
+	
+	
+	//Program
+	private GatewayService gateway;
+	
+	//METHODS
 	public User(GatewayService stub) throws RemoteException {
 		gateway = stub;
+		encryption = new EncryptionUtil();
 		
 	}
 
-	public String RegisterUser(String name, String password) throws RemoteException {
-		String response = gateway.RegisterUser(null, null, name, password);
-	
-		return response;
+	public String RegisterUser(String adminName, String adminPassword, String name, String password) throws RemoteException {
+		//make nounce
+		String timestamp = this.getTimestamp();
+		String uuid = this.getUUID();
+		
+		String pureNounce = uuid + "%" + timestamp;
+		
+		//make signature
+		
+		String pureSignature = adminName + adminPassword + name + password + pureNounce;
+		
+		byte[] signature = encryption.generateSignature(pureSignature.getBytes(UTF8));
+		
+		//encrypt
+		byte[] aName = encryption.encrypt(adminName.getBytes(UTF8));
+		byte[] aPassword = encryption.encrypt(adminPassword.getBytes(UTF8));
+
+		byte[] nName = encryption.encrypt(name.getBytes(UTF8));
+		byte[] nPassword = encryption.encrypt(password.getBytes(UTF8));
+		
+		byte[] nounce = encryption.encrypt(pureNounce.getBytes(UTF8));
+		
+		
+		List<byte[]> response = gateway.RegisterUser(aName, aPassword, nName, nPassword, nounce, signature);
+		
+		//decrypt response
+		String pureResponse = new String(encryption.decrypt(response.get(3)), UTF8);
+
+		
+		try {	
+			if(this.checkResponse(response.get(0), response.get(1), pureResponse)) {
+				return pureResponse;
+			}
+			return "Something went wrong with your request!";
+			
+		} catch (UnsupportedEncodingException e) {
+			return "[WARNING] Unsupported encoding!";
+		}catch (ParseException e) {
+			return "[WARNING] Couldn't parse some data!";
+		}catch (SignatureException e) {
+			return "[WARNING] Couldn't verify some data!";
+		}
+		
 	}
 
-	public String DeleteUser(String name) throws RemoteException {
+	public String DeleteUser(String adminName, String adminPassword, String name) throws RemoteException {
 		String response = gateway.DeleteUser(null, null, name);
 	
 		return response;
@@ -98,6 +157,83 @@ public class User {
 		return response;
 	}
 	
+	//HELPERS
+	private String getTimestamp() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+	
+	private Date convertDate(String timestamp) throws ParseException {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		return dateFormat.parse(timestamp);
+	}
+	
+	private String getUUID(){
+		return UUID.randomUUID().toString();
+	}
+	
+	private boolean checkResponse(byte[] nounce, byte[]signature, String response) throws UnsupportedEncodingException, ParseException, SignatureException {
+		this.cleanNounces();
+		//check nounce
+		
+		String pureNounce = new String(encryption.decrypt(nounce), UTF8);
+		
+		String[] parsedNounce = pureNounce.split("%");
+		
+		//checks if nounce doesnt exist and its still fresh
+		if(nounces.contains(pureNounce)) {
+			nounces.add(pureNounce);
+		}
+		else {
+			return false;
+		}
+		
+		if(!this.checkFreshness(this.convertDate(parsedNounce[1]))) {
+			return false;
+		}
+		
+		//check signature
+		String signatureGuess = response.concat(pureNounce);
+		
+		
+		return encryption.verifySignature(signatureGuess.getBytes(UTF8), signature);
+	}
+
+	private boolean checkFreshness(Date date) {
+		Date now = new Date();
+		
+		if(now.before(date)) {
+			return false;
+		}
+		
+		long timeout = TimeUnit.MILLISECONDS.convert(5, TimeUnit.MINUTES);
+		long diff = now.getTime() - date.getTime();
+		
+		
+		return   diff <= timeout;
+	}
+	
+	private void cleanNounces() throws ParseException {
+		if(this.checkStink(cleanSchedule)) {
+			for(int i = 0; i < nounces.size(); i++) {
+				String[] parsedNounce = nounces.get(i).split("%");
+				Date timestamp = this.convertDate(parsedNounce[1]);
+				if(this.checkStink(timestamp)) {
+					nounces.remove(i);
+				}
+			}
+		}
+	}
+	
+	private boolean checkStink(Date date) {
+		Date now = new Date();
+		
+		long timeout = TimeUnit.MILLISECONDS.convert(2, TimeUnit.DAYS);
+		long diff = now.getTime() - cleanSchedule.getTime();
+		
+		return   diff >= timeout;
+	}
 	
     public static void main(String[] args) throws Exception {
 
@@ -151,7 +287,7 @@ public class User {
 				case "register":
 
 					if(parsedInput.length == 3) {
-						g.RegisterUser(parsedInput[1], parsedInput[2]);
+						g.RegisterUser(parsedInput[1], parsedInput[2], parsedInput[3], parsedInput[4]);
 					}
 					else {
 						System.out.println("Unrecognized register command");
@@ -161,7 +297,7 @@ public class User {
 				case "delete":
 
 					if(parsedInput.length == 2) {
-						g.DeleteUser(parsedInput[1]);
+						g.DeleteUser(parsedInput[1], parsedInput[2], parsedInput[3]);
 					}
 					else {
 						System.out.println("Unrecognized delete command");
@@ -182,9 +318,9 @@ public class User {
 						System.out.println("login - used to start a session on the gateway\n"
 								+ ">> login username password [authenticationString]");
 						System.out.println("register - [ADMIN ONLY]register a new user in the network\n"
-								+ ">> register username password");
+								+ ">> register AdminUsername AdminPassword username password");
 						System.out.println("delete - [AMIN ONLY]delete a user from the network\n"
-								+ ">> delete username");
+								+ ">> delete AdminUsername AdminPassword username");
 						System.out.println("help - for existing commands\n"
 								+ ">> help");
 						System.out.println("exit - to exit the client program\n"
