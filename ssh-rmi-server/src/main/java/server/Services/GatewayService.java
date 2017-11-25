71bdc9e7db01fac94aaa58be2faa393b818d6c3a
Kharmico <phoenix-2000@hotmail.com>
2017-11-25 15:27:01 +0000

    Changed Strings to byte[]

--- ssh-rmi-server/src/main/java/server/Services/GatewayService.java
+++ ssh-rmi-server/src/main/java/server/Services/GatewayService.java
@@ -10,20 +10,20 @@ import javax.crypto.SecretKey;
 
 public interface GatewayService extends Remote {
 	//Admin Requests
-	public String RegisterUser(String adminUsername, String adminPassword, String name, String password) throws RemoteException;
+	public byte[] RegisterUser(byte[] adminUsername, byte[] adminPassword, byte[] name, byte[] password, byte[] nonce, byte[] signature) throws RemoteException;
 	
-	public String DeleteUser(String adminUsername, String adminPassword, String name) throws RemoteException;
+	public byte[] DeleteUser(byte[] adminUsername, byte[] adminPassword, byte[] name, byte[] nonce, byte[] signature) throws RemoteException;
 	
 	//General Requests
-	public List<ArrayList<String>> GetDeviceStatus(String username) throws RemoteException;
+	public List<ArrayList<byte[]>> GetDeviceStatus(byte[] nonce, byte[] signature) throws RemoteException;
 	
-	public List<String> GetDeviceCommands(String username, String deviceName) throws RemoteException;
+	public List<byte[]> GetDeviceCommands(byte[] deviceName, byte[] nonce, byte[] signature) throws RemoteException;
 	
-	public String SendCommand(String username, String deviceName, String command) throws RemoteException;
+	public byte[] SendCommand(byte[] deviceName, byte[] command, byte[] nonce, byte[] signature) throws RemoteException;
 	
-	public SecretKey ReplenishLogin(SecretKey userPublicKey, String username, String password, String authString) throws RemoteException;
+	public byte[] ReplenishLogin(byte[] userPublicKey, byte[] username, byte[] password, byte[] authString, byte[] nonce, byte[] signature) throws RemoteException;
 	
-	public String Login(String username, String password) throws RemoteException;
+	public byte[] Login(byte[] username, byte[] password, byte[] nonce, byte[] signature) throws RemoteException;
 	
 }
 
