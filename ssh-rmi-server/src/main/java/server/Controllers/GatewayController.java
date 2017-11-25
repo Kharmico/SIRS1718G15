ba871b7e5057a73ae2bec6eb39f3d17fbb0f9acd
Miguel Silva Mateus Belo de Carvalho <miguel.s.carvalho@tecnico.ulisboa.pt>
2017-11-25 18:05:26 +0000

    Connections between device&gateway. Use pcClient to connect the device to the GW

--- ssh-rmi-server/src/main/java/server/Controllers/GatewayController.java
+++ ssh-rmi-server/src/main/java/server/Controllers/GatewayController.java
@@ -1,6 +1,7 @@
 package server.Controllers;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.RemoteException;
@@ -13,14 +14,15 @@ import java.util.Map;
 import javax.crypto.SecretKey;
 
 import server.Services.GatewayService;
+import server.entities.Device;
 
 public class GatewayController extends UnicastRemoteObject implements GatewayService {
 
 	private static final long serialVersionUID = 1L;
 	// GatewayController Variables
-	public List<ArrayList<String>> devices = new ArrayList<ArrayList<String>>();
+	public List<Device> devices = new ArrayList<Device>();
 	private ServerSocket registerSocket;
-	private Map<String, Socket> devConnections = new HashMap<String,Socket>();
+	private Map<String, Helper> devConnections = new HashMap<String,Helper>(); //Stores the threads with the connections
 
 
 	// GatewayController Constructor
@@ -38,7 +40,8 @@ public class GatewayController extends UnicastRemoteObject implements GatewaySer
 	}
 
 	public List<ArrayList<String>> GetDeviceStatus(String username) {
-		return devices;
+		//name status type
+		return null;
 	}
 
 	public List<String> GetDeviceCommands(String username, String deviceName) {
@@ -66,19 +69,31 @@ public class GatewayController extends UnicastRemoteObject implements GatewaySer
 
 	}
 	public void startListeningDevices() throws IOException{
-		 while (true) { /* or some other condition you wish */
-	         Socket connection = registerSocket.accept(); /* will wait here */
-	         /* this code is executed when a client connects... */
+		while (true) { /* or some other condition you wish */
+			Socket connection = registerSocket.accept(); /* will wait here */
+			/* this code is executed when a client connects... */
 
-	         System.out.println("\nNow connected to " 
-	                                      + connection.getInetAddress().toString());
+			System.out.println("\nNow connected to " 
+					+ connection.getInetAddress().toString() +":" + connection.getPort());
+			
+			InputStream stream = connection.getInputStream();
+			
+			byte[] data = new byte[100];
+			int count = stream.read(data);
+			String deviceName ="";
+			
+			if ( count >0 ) {
+				deviceName = new String(data, "UTF-8");
+				System.out.println();
+			}
+			else continue;
+			
+			Helper con = new Helper(connection); /* call a nanny to take
+	                                                            care of it */
+			devConnections.put(deviceName, con); /* make++ sure you keep a ref to it, just in case */
+			con.start();						 /* tell nanny to get to work as an independent thread */
 
-	      //  ClientNanny nanny = new ClientNanny(connection); /* call a nanny to take
-	        //                                                     care of it */
-	       //  clients.add(nanny); /* make sure you keep a ref to it, just in case */
-	       //  nanny.start(); /* tell nanny to get to work as an independent thread */
-	         
-	     }
+		}
 	}
 	public int createListeningSocket(int port) throws IOException{
 
